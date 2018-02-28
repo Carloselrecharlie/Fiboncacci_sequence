cce48fac24090bdb939f65422ca7e7aea69202cf
Carloselrecharlie <G00364770@gmit.ie>
2018-02-28 14:24:15 +0000

    moving files to a different folder

--- openfile.py
+++
@@ -1,8 +0,0 @@
-f = open("data/iris.csv.txt")
-# we name it as f
-
-print(f) #outputs some info from the file
-
-print(f.read()) #this prints the content
-
-f.close() # every file should be closed before exiting CSV
