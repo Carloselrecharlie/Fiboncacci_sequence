61f2795108a310ebbaf6692991e21e448f4aaff6
Carloselrecharlie <G00364770@gmit.ie>
2018-03-28 13:11:22 +0100

    added visualization of the iris data set

--- irisDS.py
+++ irisDS.py
@@ -6,6 +6,6 @@ with open('data/iris.csv', newline=' ') as csvFile:
 
   for line in csvFile:
 
-    line = line.replace(',', ' ') #no comma separating the numbers
+    line = line.replace(',', ' ') #removes comma separating the numbers
 
     print(line[:3]+ '      ' + line[4:7]+ '      ' + line[8:11]+ '      ' + line[12:15]) 
