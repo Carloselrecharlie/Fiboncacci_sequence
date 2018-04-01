9399030009d6c810fcc1f1b0c698c7c0abccd24c
Carloselrecharlie <G00364770@gmit.ie>
2018-04-01 18:05:42 +0100

    modified iris data file and its linked python script

--- irisDS.py
+++ irisDS.py
@@ -1,11 +1,19 @@
+
+# Carlos Montés Parra
+
 import csv
 
 
-
-with open('data/iris.csv', newline=' ') as csvFile:
+with open('data/iris.csv', newline='') as csvFile:
 
   for line in csvFile:
 
     line = line.replace(',', ' ') #removes comma separating the numbers
 
+    a = float (line.split()[0]) #converts strings into values
+    b = float (line.split()[1])
+    c = float (line.split()[2])
+    d = float (line.split()[3])
+
     print(line[:3]+ '      ' + line[4:7]+ '      ' + line[8:11]+ '      ' + line[12:15]) 
+
