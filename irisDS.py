9d6a6934b5ef4c934286f2d00d97853a904f2c8e
Carloselrecharlie <G00364770@gmit.ie>
2018-04-02 15:22:26 +0100

    Added script to convert strings into values

--- irisDS.py
+++ irisDS.py
@@ -6,14 +6,19 @@ import csv
 
 with open('data/iris.csv', newline='') as csvFile:
 
+
   for line in csvFile:
+    
+    line = line.replace(',', ' ') #removes the comma separating the numbers
 
-    line = line.replace(',', ' ') #removes comma separating the numbers
-
-    a = float (line.split()[0]) #converts strings into values
-    b = float (line.split()[1])
+    a = float (line.split()[0]) #converts strings into values so I can make operations later on
+    b = float (line.split()[1]) # print(type(a)) confirms it
     c = float (line.split()[2])
     d = float (line.split()[3])
 
     print(line[:3]+ '      ' + line[4:7]+ '      ' + line[8:11]+ '      ' + line[12:15]) 
 
+
+  
+    
+  
